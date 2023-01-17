184f57f71081fd7d1f1caed3e6cd73b96fa5d228
Maxim Korovin <maximxam0n@gmail.com>
2023-01-17 09:20:42 +0300

    aod-layout

---
+++ #source/js/main.js
@@ -0,0 +1,6 @@
+//@prepros-append burger.js
+//@prepros-append ibg.js
+//@prepros-append slick.min.js
+//@prepros-append slider.js
+
+
---
+++ #source/js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$('.slider__body').slick({
+		dots: true,
+		arrows: false,
+		slidesToShow: 1,
+		accessibility: false,
+		adaptiveHeight: true,
+	});
+});  
